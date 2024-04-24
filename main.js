03016a1ec9950fed05e9d4c56ec79e887186a533
estellaaaa <estella.kinzel@gmail.com>
2024-04-24 18:39:15 +0200

    switch to express

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-const port = 3000,
-    http = require("http"),
-    httpStatus = require("http-status-codes"),
-    router = require("./router"),
-    contentTypes = require("./contentTypes"),
-    utils = require("./utils");
+const express = require("express");
+const layouts = require("express-ejs-layouts");
+const httpStatus = require("http-status-codes");
+const contentTypes = require("./contentTypes");
+const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
+const utils = require("./utils");
 
-router.get("/", (req, res) => {
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.htm);
     utils.getFile("views/index.html", res);                                                                                                                         
 });
-router.get("/productview.html", (req, res) => {
+app.get("/productview.html", (req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.html);
     utils.getFile("views/productview.html", res);
 });
-router.get("/searchview.html", (req, res) => {
+app.get("/searchview.html", (req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.html);
     utils.getFile("views/searchview.html", res);
 });
-router.get("/shoppingcart.html", (req, res) => {
+app.get("/shoppingcart.html", (req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.html);
     utils.getFile("views/shoppingcart.html", res);
 });
-router.get("/bootstrap.css", (req, res) => {
+app.get("/bootstrap.css", (req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.css);
     utils.getFile("public/css/bootstrap-4.0.0-dist/css/bootstrap.min.css", res);
 });
-http.createServer(router.handle).listen(port);
-    console.log(`The server is listening on
-    ➥ port number: ${port}`);
+
+const port = app.get("port");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
