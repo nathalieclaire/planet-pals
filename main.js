98a45159566c0dae1268c7d60ba588acd5366f96
estellaaaa <estella.kinzel@gmail.com>
2024-04-24 20:00:15 +0200

    implement error pages

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ app.set("port", process.env.PORT || 3000);
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
