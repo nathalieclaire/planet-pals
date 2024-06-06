67805fcdac2bdd862a6f4f5420533eff3ac112e6
abductedRhino <avocadorable@mailbox.org>
2024-06-07 01:51:37 +0200

    Use static htmx file, fix update

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,13 +2,11 @@ const User = require('./../models/userModel');
 
 exports.renderUsersTable = (req, res) => {
   User.find({})
-    .exec()
     .then((users) => {
       res.render('usersTable', { users: users });
     })
     .catch((error) => {
       console.error(error.message);
-      return [];
     });
 }
 
@@ -19,34 +17,46 @@ exports.renderLogin = (req, res) => {
 exports.updateUser = (req, res) => {
   const update = {};
   update[req.body.key] = req.body.value;
-  User.findByIdAndUpdate(req.body.id, {search: update})
-  .exec()
-  .then((user) => {
-    res.render('p', {value: req.body.value});
+  User.findByIdAndUpdate(req.body.id, update, { new: true })
+    .then((user) => {
+      if (user === null) {
+        console.warn('no user with id', req.body.id);
+      } else {
+        res.render('p', { key: req.body.key, value: req.body.value });
+      }
     })
     .catch((error) => {
-      console.error(error.message);
-      return [];
+      console.error(error);
+      res.render('register');
     });
 }
 
 exports.deleteUser = (req, res) => {
   User.findByIdAndDelete(req.body.id)
-    .exec()
-    .then(() => {
-      res.render('register');
+    .then((user) => {
+      if (user === null) {
+        console.warn('no user with id', req.body.id);
+      } else {
+        res.render('bye', { id: req.body.id, name: user.firstName });
+      }
     })
     .catch((error) => {
+      console.error(error);
       res.render('register');
     });
 }
 exports.renderUser = (req, res) => {
+  console.log(req.body.id)
   User.findById(req.body.id)
-    .exec()
     .then((user) => {
-      res.render('profile', { user: user });
+      if (user === null) {
+        res.render('register')
+      } else {
+        res.render('profile', { user: user });
+      }
     })
     .catch((error) => {
+      console.error(error);
       res.render('register');
     });
 }
