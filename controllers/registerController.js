fc3d9393e321791d151a96ebfb11425f81da93dd
abductedRhino <avocadorable@mailbox.org>
2024-06-12 19:12:48 +0200

    Impment password hashing

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,20 +1,25 @@
+const argon2 = require("argon2");
 const User = require('./../models/userModel');
 exports.renderRegisterView = (req, res) => {
   res.render('register', {id:''});
 }
-exports.registerUser = (req, res) => {
+async function hashPassword(plainTextPassword) {
+    return await argon2.hash(plainTextPassword);
+}
+
+exports.registerUser = async (req, res) => {
   new User(
     {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
-      password: req.body.password,
+      password: await hashPassword(req.body.password),
       address: req.body.address
     }
   )
     .save()
     .then(result => {
-      console.log(result.id)
+      console.log(result);
       res.render('register_success', {user: req.body.firstName, id: result.id});
     })
     .catch(error => {
