bf2cc6b34f54782412f3b4f20046e65242b6b754
abductedRhino <abductedrhino@mailbox.org>
2024-06-19 20:54:16 +0200

    w

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const User = require('./../models/userModel');
 const argon2 = require('argon2');
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 async function verifyPassword(hashOfPassword, plainTextPassword) {
     try {
@@ -36,7 +37,7 @@ exports.loginUser = async (req, res) => {
                 res.render('register')
             }
             console.log(user)
-            const result = await verifyPassword(user.password, req.body.pass);
+            const result = await verifyPassword(user.password, req.body.password);
             if (result === 'correct') {
                 console.log('correct password');
                 res.render('index', {username: user.firstName})
@@ -97,3 +98,36 @@ exports.renderUser = (req, res) => {
       res.render('register');
     });
 }
+passport.use(new LocalStrategy(
+    {passReqToCallback: true },
+    function(req, email, pass, done) {
+        console.log("made it!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        User.findOne({ email: email })
+            .then((user) => verify(req, user, done))
+            .catch((err) => done(err));
+    }
+));
+
+async function verify(req, user, done) {
+    console.log("aslkdfalkdsjfldsajflksadjflasdjflk")
+    if (!user) {
+        return done(null, false);
+    }
+    if (await verifyPassword(user.password, req.body.password) === 'correct') {
+        return done(null, user);
+    }
+    return done(null, false);
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user.email);
+});
+
+passport.deserializeUser(async (email, done) => {
+    try {
+        const user = await User.findOne({email: email});
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
