aeb56f429a093c6abdbaa233019280db25b9f76e
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2024-06-25 17:05:03 +0200

    updated backlog, added status report

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/userModel');
+
+module.exports = function (passport) {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+      const isMatch = await user.isValidPassword(password);
+      if (!isMatch) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+};
+
+router.post(user.email, '/login', passport.authenticate('local', {
+    successRedirect: '/user/${user.email}',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+  
---
+++ reports/status_report_08.md
@@ -0,0 +1,42 @@
+# Report sprint 10
+
+## Your project name
+
+planet-pals - [v10.0.0](https://github.com/nathalieclaire/planet-pals/releases/tag/v10.0.0)
+
+## Team members of PlanetPals
+
+* Nathalie Claire Huppert - [nathalieclaire/planet-pals](https://github.com/nathalieclaire/planet-pals)
+* Estella Kinzel - [(fork) estellaaaa/planet-pals](https://github.com/estellaaaa/planet-pals)
+* Tobias Bayer - [(fork) abductedRhino/planet-pals](https://github.com/abductedRhino/planet-pals)
+
+## Tasks for this sprint
+
+* Implement a RESTful API for your application, based on an appropriate user story.
+
+## The sprint backlog as a text file
+
+```
+Product Owner: Nathalie
+Scrum Master: Estella
+
+BACKLOG
+
+
+IN PROGRESS
+
+
+DONE
+
+
+```
+
+## The stories you've implemented in this sprint
+
+
+
+## Reflection
+
+process and external issues that prevented you from working towards the sprint goal:
+
+steps to improve the situation for upcoming sprint:
