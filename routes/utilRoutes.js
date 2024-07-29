38ff74ad2e63f7744ee120cd0b74aac40eccba43
abductedRhino <abductedrhino@mailbox.org>
2024-07-29 20:24:59 +0200

    feat: add a hello world as a background
    To build the background, run npm build-bg. The background is served
    via a route in routes/utilRoutes.js

--- routes/utilRoutes.js
+++ routes/utilRoutes.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const utils = require("../utils");
 
 router.get("/bootstrap.css", (req, res) => {
-    res.writeHead(200, contentTypes.css);
+    res.writeHead(200, {"Content-Type": "text/css"});
     utils.getFile("public/css/bootstrap-4.0.0-dist/css/bootstrap.min.css", res);
 });
 router.get("/htmx.min.js", (req, res) => {
-    res.writeHead(200, contentTypes.js);
+    res.writeHead(200, {"Content-Type": "text/js"});
     utils.getFile("public/js/htmx.min.js", res);
 });
 
